567cef4bc130d5f3779211c70510b6f089ee8154
Dawid Kaczmarek <d.kaczmarek29@gmail.com>
2018-04-25 20:49:45 +0200

    Add code into navigation.js

--- javascript-compailed/navigation.js
+++ javascript-compailed/navigation.js
@@ -3,17 +3,37 @@
 var navLinks = document.getElementsByClassName("navbar__list--item");
 console.log(navLinks);
 
-/*Create array*/
+/*Create array link*/
 var arrNavLinks = [].slice.call(navLinks);
 
-function srcollTo() {
-	console.log('jest ok');
+/*Get off setTop sections*/
+
+var sections = document.getElementsByTagName('section');
+var arrSections = [].slice.call(sections).slice(1);
+
+/*Get offsetTop sections*/
+
+function getOffSetTop(arr) {
+
+	var offSetPositions = [];
+
+	for (var i = 0; i < arr.length; i++) {
+		offSetPositions.push(arr[i].offsetTop);
+	}
+
+	return offSetPositions;
 }
 
-/*Create Events*/
+var arrOfSetTopSections = getOffSetTop(arrSections);
+
+/*On click event*/
+function srcollTo(event) {
+	console.log(event);
+}
+
 arrNavLinks.forEach(function (element) {
 	element.addEventListener("click", function (e) {
 		e.preventDefault();
-		srcollTo();
+		srcollTo(e);
 	}, false);
 });
