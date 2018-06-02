ac863e0e811aec8432ecc2a5459c180e4f398dc7
Dawid Kaczmarek <d.kaczmarek29@gmail.com>
2018-06-02 13:32:07 +0200

    Add code into gallery.js

--- javascript-compailed/gallery.js
+++ javascript-compailed/gallery.js
@@ -7,40 +7,75 @@ var galleryBtnAll = [].slice.call(galleryBtn).slice(0, 1);
 var arrGalleryBtn = [].slice.call(galleryBtn).slice(1);
 var arrAllGalleryBtn = galleryBtnAll.concat(arrGalleryBtn);
 
-/*add click event */
+/*add click events */
 
 arrGalleryBtn.forEach(function (btn, btnGalleryIndex) {
-	btn.addEventListener("click", function (e) {
-		unActiveGalleryBtn();
-		activeGalleryBtn(e.target);
-		showActiveGallery(btnGalleryIndex);
-	}, false);
+  btn.addEventListener("click", function (e) {
+    unActiveGalleryBtn();
+    unActiveGallerySec();
+    hideMoreProjectBtn();
+    activeGalleryBtn(e.target);
+    showActiveGallerySec(btnGalleryIndex);
+  }, false);
 });
 
 galleryBtnAll[0].addEventListener("click", function (e) {
-	unActiveGalleryBtn();
-	activeGalleryBtn(e.target);
+  unActiveGalleryBtn();
+  unActiveGallerySec();
+  showActiveGallerySec([0]);
+  activeGalleryBtn(e.target);
+  showMoreProjectBtn();
 }, false);
+
+/* add  load evetn */
+
+window.addEventListener("load", function (e) {
+  unActiveGallerySec();
+  showActiveGallerySec([0]);
+});
 
 /* add and remove class  active in gallery navigation */
 
 function activeGalleryBtn(btn, btnGalleryIndex) {
-	btn.setAttribute("class", "nav-gallery__button active-button");
+  btn.setAttribute("class", "nav-gallery__button active-button");
 }
 
 function unActiveGalleryBtn() {
-	arrAllGalleryBtn.forEach(function (btn) {
-		btn.setAttribute("class", "nav-gallery__button");
-	});
+  arrAllGalleryBtn.forEach(function (btn) {
+    btn.setAttribute("class", "nav-gallery__button");
+  });
 }
 
-/*Get gallery sections */
+/* Get gallery sections */
 
 var gallerySec = document.getElementsByClassName("gallery__section");
 var arrGallerySec = [].slice.call(gallerySec);
 
-/* Show active gallery section */
+/* Get more project button */
 
-function showActiveGallery(btnGalleryIndex) {
-	console.log(arrGallerySec[btnGalleryIndex]);
+var moreProjectBtn = document.getElementById("gallery__button");
+
+/* Show more project button */
+
+function showMoreProjectBtn() {
+  moreProjectBtn.setAttribute("style", "display: inline-block;");
+}
+
+/* Hide more project button */
+
+function hideMoreProjectBtn() {
+  moreProjectBtn.setAttribute("style", "display: none;");
+}
+
+/* Hide gallery sections */
+
+function unActiveGallerySec() {
+  arrGallerySec.forEach(function (section) {
+    section.setAttribute("style", "display:none;");
+  });
+}
+
+/* Show active gallery section */
+function showActiveGallerySec(btnGalleryIndex) {
+  arrGallerySec[btnGalleryIndex].setAttribute("style", "display:flex;");
 }
--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,46 +1,87 @@
 /* Get gallery buttons */
 
 const galleryBtn = document.getElementsByClassName("nav-gallery__button");
-const galleryBtnAll = [].slice.call(galleryBtn).slice(0,1);
+const galleryBtnAll = [].slice.call(galleryBtn).slice(0, 1);
 const arrGalleryBtn = [].slice.call(galleryBtn).slice(1);
 const arrAllGalleryBtn = galleryBtnAll.concat(arrGalleryBtn);
 
-
-/*add click event */
+/*add click events */
 
 arrGalleryBtn.forEach((btn, btnGalleryIndex) => {
-    btn.addEventListener("click", function(e) {
-				unActiveGalleryBtn();    
-				activeGalleryBtn(e.target);
-				showActiveGallery(btnGalleryIndex);
-    }, false);
+  btn.addEventListener(
+    "click",
+    function(e) {
+      unActiveGalleryBtn();
+      unActiveGallerySec();
+      hideMoreProjectBtn();
+      activeGalleryBtn(e.target);
+      showActiveGallerySec(btnGalleryIndex);
+    },
+    false
+  );
 });
 
-galleryBtnAll[0].addEventListener("click", function(e){
-	unActiveGalleryBtn();
-	activeGalleryBtn(e.target);
-},false);
+galleryBtnAll[0].addEventListener(
+  "click",
+  function(e) {
+    unActiveGalleryBtn();
+    unActiveGallerySec();
+    showActiveGallerySec([0]);
+    activeGalleryBtn(e.target);
+    showMoreProjectBtn();
+  },
+  false
+);
 
+/* add  load evetn */
+
+window.addEventListener("load", function(e) {
+  unActiveGallerySec();
+  showActiveGallerySec([0]);
+});
 
 /* add and remove class  active in gallery navigation */
 
 function activeGalleryBtn(btn, btnGalleryIndex) {
-	btn.setAttribute("class", "nav-gallery__button active-button");
+  btn.setAttribute("class", "nav-gallery__button active-button");
 }
 
 function unActiveGalleryBtn() {
-	arrAllGalleryBtn.forEach((btn) => {
-		btn.setAttribute("class", "nav-gallery__button");
-	})
+  arrAllGalleryBtn.forEach(btn => {
+    btn.setAttribute("class", "nav-gallery__button");
+  });
 }
 
-/*Get gallery sections */
+/* Get gallery sections */
 
 const gallerySec = document.getElementsByClassName("gallery__section");
 const arrGallerySec = [].slice.call(gallerySec);
 
-/* Show active gallery section */
+/* Get more project button */
 
-function showActiveGallery(btnGalleryIndex) {
-	console.log(arrGallerySec[btnGalleryIndex]);
+const moreProjectBtn = document.getElementById("gallery__button");
+
+/* Show more project button */
+
+function showMoreProjectBtn() {
+  moreProjectBtn.setAttribute("style", "display: inline-block;");
+}
+
+/* Hide more project button */
+
+function hideMoreProjectBtn() {
+  moreProjectBtn.setAttribute("style", "display: none;");
+}
+
+/* Hide gallery sections */
+
+function unActiveGallerySec() {
+  arrGallerySec.forEach(section => {
+    section.setAttribute("style", "display:none;");
+  });
+}
+
+/* Show active gallery section */
+function showActiveGallerySec(btnGalleryIndex) {
+  arrGallerySec[btnGalleryIndex].setAttribute("style", "display:flex;");
 }
